b3c7e3605f19fb5d97f994decc24dabd46fdf6ba
Gennadii <atlantagenafes@gmail.com>
2022-10-21 17:36:12 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,170 @@
+var cubeMapArr1 = [
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis4,szn",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis2,szn",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis5,szn",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis6,szn",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis1,szn",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CHRn8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis3,szn",
+];
+
+var cubeMapArr2 = [
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis4",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis2",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis5",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis6",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis1",
+  "https://nafta.starconnect-ce.i.daimler.com/iris/iris.jpg?COSY-EU-100-1713d0VXqaSFqtyO67PobzIr3eWsrrCsdRRzwQZQ9vZbMw3SGtGyutsd2HTcUfp8fXGEuiRJ0l3IrOB2NW2bApjtwI5uG5uQC3aM1kzNH%25km7j8hfhKVk0Q%25vqeU%25yLRsGWYaxCXWrH1zMdn8w7hnoiZK%25gM4FvTQTg9LU26PDaGqSeWFmItsd9kVcUfAyYXOc6VRjtirH1CB%25n8w5zEoiZHiNM4F8CwTg9jze6PDePoSeWsKMtsLV79MOuljcVLHvGEVdhcfWF002ROxEnfxXr1RjijhWh5DvaAFCDGp04U4aLRg7vxb=undefined&fb=1&BKGND=12&IMGT=A27&pov=bis3",
+];
+
+var equirectangularOne =
+  "https://mediaserver.mbusa.com/proxy/mbusa/2020/s450v?client=mb&brand=mbusa&resp=err_status%2Cjpeg&quality=90&vehicle=2020_s450v&pov=centerpano%2Ccgd&paint=2_040&fabric=4_201&sa=0_729&width=4500&height=2250";
+
+var trimBtnOne = document.querySelector(".switch-trim-1");
+var trimBtnTwo = document.querySelector(".switch-trim-2");
+var trimBtnThree = document.querySelector(".switch-trim-3");
+var leftBtn = document.querySelector(".left");
+var rightBtn = document.querySelector(".right");
+var upBtn = document.querySelector(".up");
+var downBtn = document.querySelector(".down");
+var zoomInBtn = document.querySelector(".in");
+var zoomOutBtn = document.querySelector(".out");
+var container = document.querySelector(".viewer-container");
+
+var panorama, viewer, camera, texture;
+
+var showBBD = true;
+
+function setPanorama(assets) {
+  if (showBBD) {
+    panorama = new PANOLENS.CubePanorama(assets);
+  } else {
+    panorama = new PANOLENS.ImagePanorama(assets);
+  }
+  // panorama.addEventListener( 'onLoad', () => {
+  //   texture = panorama.getTexture();
+  // });
+}
+
+function addViewer() {
+  viewer = new PANOLENS.Viewer({
+    container: container,
+    controlBar: false,
+  });
+  viewer.OrbitControls.maxFov = 80;
+  viewer.OrbitControls.minFov = 50;
+  viewer.camera.position.set(0, 0.12, -0.5);
+}
+
+function updatePanorama(arr) {
+  viewer.animate();
+  viewer.remove(panorama);
+  panorama.dispose();
+  setPanorama(arr);
+  viewer.add(panorama);
+  if (showBBD) {
+    viewer.camera.position.set(0, 0.12, -0.5);
+  } else {
+    viewer.camera.position.set(0.96, 0.23, 0.14);
+  }
+  // panorama.material.map = NEW_TEXUTRE;
+  // texture = panorama.getTexture();
+  // texture.minFilter = THREE.minFilter;
+}
+
+function clickOne() {
+  showBBD = true;
+  updatePanorama(cubeMapArr1);
+}
+
+function clickTwo() {
+  showBBD = true;
+  updatePanorama(cubeMapArr2);
+}
+
+function clickThree() {
+  if (showBBD) {
+    showBBD = false;
+    updatePanorama(equirectangularOne);
+  } else {
+    showBBD = true;
+    updatePanorama(cubeMapArr2);
+  }
+}
+
+function zoom(direction) {
+  var currentZoom = viewer.camera.fov;
+  var newZoom = 0;
+  switch (direction) {
+    case "in":
+      newZoom = currentZoom - 10;
+      if (newZoom < 30) newZoom = 30;
+      break;
+    case "out":
+      newZoom = currentZoom + 10;
+      if (newZoom > 90) newZoom = 90;
+      break;
+  }
+  viewer.setCameraFov(newZoom);
+}
+
+var ROTATION_POSITION = 0.01;
+var rotating = false,
+  rotationInterval;
+
+function rotateLeftRigth(direction) {
+  if (rotating) {
+    let rp = direction === "left" ? -ROTATION_POSITION : ROTATION_POSITION;
+    rotationInterval = setInterval(() => {
+      viewer.OrbitControls.rotateLeft(rp);
+    }, 15);
+  }
+  console.log("-", viewer.getCamera());
+}
+
+function rotateUpDown(direction) {
+  if (rotating) {
+    let rp = direction === "up" ? -ROTATION_POSITION : ROTATION_POSITION;
+    rotationInterval = setInterval(() => {
+      viewer.OrbitControls.rotateUp(rp);
+    }, 15);
+  }
+}
+
+function startRotation(dir) {
+  clearInterval(rotationInterval);
+  if (!rotating) {
+    rotating = true;
+    if (dir === "left" || dir === "right") {
+      rotateLeftRigth(dir);
+    }
+    if (dir === "up" || dir === "down") {
+      rotateUpDown(dir);
+    }
+  }
+}
+
+function stopRotation() {
+  if (rotating) {
+    rotating = false;
+    clearInterval(rotationInterval);
+  }
+}
+
+setPanorama(cubeMapArr2);
+addViewer();
+viewer.add(panorama);
+
+trimBtnOne.onclick = clickOne;
+trimBtnTwo.onclick = clickTwo;
+trimBtnThree.onclick = clickThree;
+
+leftBtn.addEventListener("mousedown", () => startRotation("left"));
+leftBtn.addEventListener("mouseup", stopRotation);
+rightBtn.addEventListener("mousedown", () => startRotation("right"));
+rightBtn.addEventListener("mouseup", stopRotation);
+upBtn.addEventListener("mousedown", () => startRotation("up"));
+upBtn.addEventListener("mouseup", stopRotation);
+downBtn.addEventListener("mousedown", () => startRotation("down"));
+downBtn.addEventListener("mouseup", stopRotation);
+zoomInBtn.onclick = () => zoom("in");
+zoomOutBtn.onclick = () => zoom("out");
